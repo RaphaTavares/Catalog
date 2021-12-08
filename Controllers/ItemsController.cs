a1973798badc0de483ce7023edab9ee6d046ef72
Raphael Tavares <raphael_tavares2000@hotmail.com>
2021-12-08 01:40:36 -0300

    Implement CreateItem route and method

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -39,6 +39,24 @@ namespace Catalog.Controllers
             }
             return item.AsDto();
         }
+
+        //POST /items
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            //duvida: pra que tudo isso irmão?
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
+        }
     }
 
     
---
+++ Dtos/CreateItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace catalog.Dtos
+{
+    //using record instead of classes for DTO's
+    public record CreateItemDto
+    {
+        //using init instead of set because with that you can only set the value when initializing the object
+        [Required]
+        public String Name { get; init; }
+        [Required]
+        [Range(1, 1000)]
+        public decimal Price { get; init; }
+    }
+}
