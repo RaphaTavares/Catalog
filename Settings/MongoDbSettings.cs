1c8cc0376aa6d427571a80d09701930e48680c54
Raphael Tavares <raphael_tavares2000@hotmail.com>
2021-12-10 19:20:42 -0300

    Create MongDb repository

---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace catalog.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return $"mongodb"
+            }
+        }
+    }
+}
